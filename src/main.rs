90563c98033ceecc6abb65204aeac26426d393a4
x-vmaier <valentin16.maier@gmail.com>
2025-09-03 15:15:56 +0200

    Remove unwanted outer loop

--- src/main.rs
+++ src/main.rs
@@ -6,31 +6,29 @@ use skeleton::Skeleton;
 
 #[macroquad::main("RustIK")]
 async fn main() {
+    let center = vec2(screen_width() / 2.0, screen_height() / 2.0);
+
+    // Create skeleton
+    let mut skeleton = Skeleton::new(Some(center));
+    skeleton.add_bone(100.0, 0.0);
+    skeleton.add_bone(100.0, 0.0);
+
     loop {
-        let center = vec2(screen_width() / 2.0, screen_height() / 2.0);
+        // Get mouse pos as vector
+        let target = Vec2::new(mouse_position().0, mouse_position().1);
 
-        // Create skeleton
-        let mut skeleton = Skeleton::new(Some(center));
-        skeleton.add_bone(100.0, 0.0);
-        skeleton.add_bone(100.0, 0.0);
-
-        loop {
-            // Get mouse pos as vector
-            let target = Vec2::new(mouse_position().0, mouse_position().1);
-
-            // Move skeleton to mouse pos
-            if is_key_pressed(KeyCode::C) {
-                skeleton.set_origin(target);
-            }
-
-            // Update skeleton
-            skeleton.update(target);
-
-            // Render
-            clear_background(BLACK);
-            skeleton.render();
-
-            next_frame().await;
+        // Move skeleton to mouse pos
+        if is_key_pressed(KeyCode::C) {
+            skeleton.set_origin(target);
         }
+
+        // Update skeleton
+        skeleton.update(target);
+
+        // Render
+        clear_background(BLACK);
+        skeleton.render();
+
+        next_frame().await;
     }
 }

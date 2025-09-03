2af1aef0de2f50fb8091944025559bd039812288
x-vmaier <valentin16.maier@gmail.com>
2025-09-03 09:56:27 +0200

    Create macroquad window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("RustIK")]
+async fn main() {
+    loop {
+        clear_background(BLACK);
+        draw_line(screen_width() / 2.0, screen_height() / 2.0, mouse_position().0, mouse_position().1, 10.0, WHITE);
+        next_frame().await
+    }
 }

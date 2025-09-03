204ed91e92ac75a3a1fbd74dfb373abbccfda4ca
x-vmaier <valentin16.maier@gmail.com>
2025-09-03 10:35:03 +0200

    Render bones with a joint

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,29 @@
+mod bone;
+mod joint;
+
+use bone::Bone;
+use joint::Joint;
 use macroquad::prelude::*;
 
 #[macroquad::main("RustIK")]
 async fn main() {
+    let center = vec2(screen_width() / 2.0, screen_height() / 2.0);
+
+    // Create bones
+    let bone1 = Bone::new(center, 100.0, 0.5);
+    let bone2 = Bone::new(bone1.end(), 80.0, 1.0);
+
+    // Link bones
+    let joint = Joint::new(&bone1, &bone2);
+
     loop {
         clear_background(BLACK);
-        draw_line(screen_width() / 2.0, screen_height() / 2.0, mouse_position().0, mouse_position().1, 10.0, WHITE);
+
+        // Render skeleton
+        bone1.render();
+        bone2.render();
+        joint.render();
+
         next_frame().await
     }
 }

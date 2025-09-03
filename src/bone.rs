c457231835e5b13d7c6124921f0e590370edc77f
x-vmaier <valentin16.maier@gmail.com>
2025-09-03 10:31:03 +0200

    Implement bone creation and rendering

--- src/bone.rs
+++ src/bone.rs
@@ -5,3 +5,26 @@ pub struct Bone {
     length: f32,
     angle: f32,
 }
+
+impl Bone {
+    pub fn new(start: Vec2, length: f32, angle: f32) -> Self {
+        Self {
+            start,
+            length,
+            angle,
+        }
+    }
+
+    pub fn end(&self) -> Vec2 {
+        self.start
+            + Vec2::new(
+                self.length * self.angle.cos(),
+                self.length * self.angle.sin(),
+            )
+    }
+
+    pub fn render(&self) {
+        let end = self.end();
+        draw_line(self.start.x, self.start.y, end.x, end.y, 4.0, WHITE);
+    }
+}

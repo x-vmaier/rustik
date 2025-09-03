96b5e6d7b4c877a19fc107b967360788709bca08
x-vmaier <valentin16.maier@gmail.com>
2025-09-03 10:32:20 +0200

    Implement joint creation and rendering

--- src/joint.rs
+++ src/joint.rs
@@ -1,6 +1,18 @@
 use crate::bone::Bone;
+use macroquad::prelude::*;
 
 pub struct Joint<'a> {
     parent: &'a Bone,
     child: &'a Bone,
+}
+
+impl<'a> Joint<'a> {
+    pub fn new(parent: &'a Bone, child: &'a Bone) -> Self {
+        Self { parent, child }
+    }
+
+    pub fn render(&self) {
+        let pos = self.parent.end();
+        draw_circle(pos.x, pos.y, 10.0, GREEN);
+    }
 }
